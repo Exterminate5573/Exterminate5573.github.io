cc7569853d735602d691c5c16cb40d3e7f9fd028
Exterminate <exterminate5573@gmail.com>
2025-02-04 12:45:04 +1100

    Added Flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { Button, Alert, Card, Navbar, NavBrand, NavLi, NavUl, NavHamburger, Toast, DarkMode } from 'flowbite-svelte';
+    
+    import { page } from '$app/stores';
+    $: activeUrl = $page.url.pathname;
+</script>
+
+<Navbar let:hidden let:toggle color="form" class="fixed top-0 left-0 right-0 z-50">
+  <NavBrand href="/">
+    <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
+    <span class="self-center whitespace-nowrap text-xl font-semibold">Exterminate</span>
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden} {activeUrl}>
+    <NavLi href="/">Home</NavLi>
+    <NavLi href="/projects">Projects</NavLi>
+    <NavLi href="/socials">Socials</NavLi>
+  </NavUl>
+  <DarkMode /> 
+</Navbar>
+
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,16 @@
+import flowbitePlugin from 'flowbite/plugin'
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {"50":"#fef2f2","100":"#fee2e2","200":"#fecaca","300":"#fca5a5","400":"#f87171","500":"#ef4444","600":"#dc2626","700":"#b91c1c","800":"#991b1b","900":"#7f1d1d"}
+      }
+    }
   },
 
-  plugins: []
+  plugins: [flowbitePlugin]
 } satisfies Config;
